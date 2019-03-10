ad0556760cf757157345e7254c924094744314c8
YashBoss <yashksri199747@gmail.com>
2019-03-10 23:57:58 +0530

    Updates

---
+++ precache-manifest.4c0394e17f1c7335e3073f46147bef34.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9c7dddbc39ed9f44227",
+    "url": "/hackgridquiz/static/css/main.185d03bb.chunk.css"
+  },
+  {
+    "revision": "d9c7dddbc39ed9f44227",
+    "url": "/hackgridquiz/static/js/main.dd1fdcad.chunk.js"
+  },
+  {
+    "revision": "516ab5086ead9810a056",
+    "url": "/hackgridquiz/static/js/runtime~main.89cdc4c0.js"
+  },
+  {
+    "revision": "d965ded1863ec576c5eb",
+    "url": "/hackgridquiz/static/css/2.e00c9b82.chunk.css"
+  },
+  {
+    "revision": "d965ded1863ec576c5eb",
+    "url": "/hackgridquiz/static/js/2.7a61c53d.chunk.js"
+  },
+  {
+    "revision": "a80bfdd5aa3f61b3f15fa6a78543d096",
+    "url": "/hackgridquiz/static/media/hackgridLogo.a80bfdd5.png"
+  },
+  {
+    "revision": "182b63bbb1cecab4177a7b7abdfbde1e",
+    "url": "/hackgridquiz/static/media/Logo.182b63bb.png"
+  },
+  {
+    "revision": "eca3a5d8843aae7eb3b6813b21444c68",
+    "url": "/hackgridquiz/static/media/quiz.eca3a5d8.png"
+  },
+  {
+    "revision": "a3a29c2ddf2da397969498b4bf8702be",
+    "url": "/hackgridquiz/static/media/Keyboard.a3a29c2d.png"
+  },
+  {
+    "revision": "7293bd81696cbd30bfdf03749328b832",
+    "url": "/hackgridquiz/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/QuizPortal/precache-manifest.867e68f867ea8acd69ba1656724580a0.js"
+  "/hackgridquiz/precache-manifest.4c0394e17f1c7335e3073f46147bef34.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/QuizPortal/index.html", {
+workbox.routing.registerNavigationRoute("/hackgridquiz/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
